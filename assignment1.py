b79bb9dc7399921c8b80ed29c42866cab57476b1
udithm <udithm123@gmail.com>
2020-08-15 16:38:21 +0530

    added multiplication

--- assignment1.py
+++ assignment1.py
@@ -2,14 +2,17 @@ def addition(a,b):
 	return a+b
 
 def subtraction(a,b):
-	return a-b;
+	return a-b
 
+def multiplication(a,b):
+	return a*b
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(multiplication(a,b))
 
 
 '''
